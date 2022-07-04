814e52367fcd701bcaa09ccb1a01b86095638f76
Graham Strickland <grahamhstrickland@gmail.com>
2022-07-04 13:52:35 +0200

    Added ch09 to repository; added testOrderedArrayListType.cpp; compiled with clang++ for test purposes

---
+++ ch09/orderedArrayListType.h
@@ -0,0 +1,92 @@
+#ifndef ORDERED_ARRAY_LIST_TYPE
+#define ORDERED_ARRAY_LIST_TYPE
+
+#include "arrayListType.h"
+
+using namespace std;
+
+template <class elemType>
+class orderedArrayListType: public arrayListType<elemType>
+{
+public:
+    int binarySearch(const elemType& item) const;
+    void insertOrd(const elemType& item);
+    orderedArrayListType(int size = 100);
+};
+
+template <class elemType>
+int orderedArrayListType<elemType>::binarySearch
+                            (const elemType& item) const
+{
+    int first = 0;
+    int last = this->length - 1;
+    int mid;
+
+    bool found = false;
+
+    while (first <= last && !found) 
+    {
+        mid = (first + last) / 2;
+
+        if (this->list[mid] == item)
+            found = true;
+        else if (this->list[mid] > item)
+            last = mid - 1;
+        else
+            first = mid + 1;
+    }
+
+    if (found)
+        return mid;
+    else
+        return -1;
+}//end binarySearch
+
+template <class elemType>
+void orderedArrayListType<elemType>::insertOrd(const elemType& item)
+{
+    int first = 0;
+    int last = this->length - 1;
+    int mid;
+
+    bool found = false;
+
+    if (this->length == 0)    //the list is empty
+    {
+        this->list[0] = item;
+        this->length++;
+    }
+    else if (this->length == this->maxSize)
+        cerr << "Cannot insert into a full list." << endl;
+    else
+    {
+        while (first <= last && !found)
+        {
+            mid = (first + last) / 2;
+
+            if (this->list[mid] == item)
+                found = true;
+            else if (this->list[mid] > item)
+                last = mid - 1;
+            else
+                first = mid + 1;
+        }//end while
+         
+        if (found)
+            cerr << "The insert item is already in the list. "
+                 << "Duplicates are not allowed." << endl;
+        else
+        {
+            if (this->list[mid] < item)
+                mid++;
+
+            this->insertAt(mid, item);
+        }
+    }
+}//end insertOrd
+ 
+template <class elemType>
+orderedArrayListType<elemType>::orderedArrayListType(int size)
+    : arrayListType<elemType>(size)
+{}
+#endif //ORDERED_ARRAY_LIST_TYPE
---
+++ ch09/testOrderedArrayListType.cpp
@@ -0,0 +1,43 @@
+//This is a driver program to test the ADT orderedArrayListType
+//  which implements an ordered array-based list with binary
+//  search method.
+
+#include <iostream> // std::cout
+#include <string>   // std::to_string
+#include "orderedArrayListType.h"
+
+int main()
+{
+    orderedArrayListType<int> list;
+    int elem, index;
+    
+    //Test insertOrd().
+    list.insertOrd(63);
+    list.insertOrd(45);
+    list.insertOrd(32);
+    list.insertOrd(98);
+    list.insertOrd(46);
+    list.insertOrd(57);
+    list.insertOrd(28);
+    list.insertOrd(100);
+
+    //Test binarySearch().
+    elem = 90;
+    index = list.binarySearch(elem);
+    std::cout << "Element " << elem << (index == -1 ? " does not occur in list."
+            : " occurs at index " + std::to_string(index) + " in list.") << '\n';
+    elem = 57;
+    index = list.binarySearch(elem);
+    std::cout << "Element " << elem << (index == -1 ? " does not occur in list."
+            : " occurs at index " + std::to_string(index) + " in list.") << '\n';
+    elem = 63;
+    index = list.binarySearch(elem);
+    std::cout << "Element " << elem << (index == -1 ? " does not occur in list."
+            : " occurs at index " + std::to_string(index) + " in list.") << '\n';
+    elem = 120;
+    index = list.binarySearch(elem);
+    std::cout << "Element " << elem << (index == -1 ? " does not occur in list."
+            : " occurs at index " + std::to_string(index) + " in list.") << '\n';
+
+    return 0;
+}
