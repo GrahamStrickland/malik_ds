234d81083bb937de0793449761026af8f0206e1f
Graham Strickland <grahamhstrickland@gmail.com>
2022-07-28 12:30:27 +0200

    Added testStateHashT.cpp; compiled with clang++

---
+++ ch09/testStateHashT.cpp
@@ -0,0 +1,11 @@
+//This is a driver program to test the ADT's hashT and stateData.
+
+#include "hashT.h"
+#include "stateData.h"
+
+int main()
+{
+    std::cout << "Program working correctly.\n";
+
+    return EXIT_SUCCESS;
+}
